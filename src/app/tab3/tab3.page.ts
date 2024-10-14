ff82db86dd51efcf51f786805f68885c89d0c006
Antonio-sharp-plus <antoniozarra8@gmail.com>
2024-10-14 12:13:08 -0300

    Botones de Sliding
    Agregué la funcionalidad para quitar un item de la lista.
    El botón de descargar está, pero hay que hacer la funcionalidad.

--- src/app/tab3/tab3.page.ts
+++ src/app/tab3/tab3.page.ts
@@ -10,11 +10,20 @@ import { FavoritosService } from '../services/favoritos.service';
 export class Tab3Page {
   personajes_favoritos: any[] = [];
 
-  constructor(private router: Router, private favoritosService: FavoritosService) {
+  constructor(
+    private router: Router,
+    private favoritosService: FavoritosService
+    ) {
     this.personajes_favoritos = this.favoritosService.obtenerFavoritos(); // Cargar favoritos al inicializar
   }
 
   goToCharacterDetail(id: number) {
     this.router.navigate(['/tabs/tab2', id]);
+  }
+
+  quitarFavorito(id: number){
+    this.personajes_favoritos = this.personajes_favoritos.filter(p => p.id !== id);
+    this.favoritosService.actualizarFavoritos(this.personajes_favoritos);
+    console.log("Personaje eliminado")
   }
 }

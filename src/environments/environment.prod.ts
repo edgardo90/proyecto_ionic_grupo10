39a0e8262bc2b69d0164cb792e11d58127847594
Antonio-sharp-plus <antoniozarra8@gmail.com>
2024-10-31 12:08:41 -0300

    Update environment.prod.ts

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,16 @@
 export const environment = {
-  production: true
+  production: true,
+  firebaseConfig: {
+    apiKey: "AIzaSyC3Hj2ce05uTHtDJcDgrLfL3oeowSBRJVw",
+    authDomain: "presopuestoapp.firebaseapp.com",
+    projectId: "presopuestoapp",
+    storageBucket: "presopuestoapp.appspot.com",
+    messagingSenderId: "815859542479",
+    appId: "1:815859542479:web:d47a69e82fe880224b4aba"
+  },
+  apiConfig: {
+    apiUrl: "https://superheroapi.com/api/",
+    token: "b2db602f073241ccf79529027610df4d/",
+    endPoint: "search/"
+  }
 };
